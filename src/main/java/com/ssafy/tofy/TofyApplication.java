878e80fc40a40a4e220f67acd7116bfb53d7939a
ekclstkfka5 <ekclstkfka5@gmail.com>
2023-05-17 17:45:26 +0900

    초기 프로젝트 세팅 완료

---
+++ src/main/java/com/ssafy/tofy/TofyApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.tofy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TofyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TofyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssafy/tofy/TofyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssafy.tofy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TofyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
