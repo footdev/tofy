7956f0254713aacde7f283186d22858bd8ab6f1f
KodaHye <kodhdh09@gmail.com>
2023-05-18 11:31:51 +0900

    feat: 여행지 REST API 구현 중

---
+++ src/main/java/com/ssafy/tofy/attraction/dto/Gugun.java
@@ -0,0 +1,10 @@
+package com.ssafy.tofy.attraction.dto;
+
+import lombok.Data;
+
+@Data
+public class Gugun {
+	private String gugunCode;  //구군 코드
+	private String sidoCode;   //시도 코드
+	private String gugunName;	   //구군 이름
+}
---
+++ src/main/java/com/ssafy/tofy/attraction/dto/Sido.java
@@ -0,0 +1,10 @@
+package com.ssafy.tofy.attraction.dto;
+
+import lombok.Data;
+
+@Data
+public class Sido {
+	private String sidoCode;  //시도 코드
+	private String name;      //시도 이름
+}
+
