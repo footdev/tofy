06aa73074bcd9fa8c066bc0c3c9b374d497dad99
KodaHye <kodhdh09@gmail.com>
2023-05-23 16:20:44 +0900

    feat: 여행계획 생성 기능 구현 중

---
+++ src/main/java/com/ssafy/tofy/plan/service/PlanService.java
@@ -0,0 +1,29 @@
+package com.ssafy.tofy.plan.service;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Service;
+
+import com.ssafy.tofy.plan.dto.Plan;
+import com.ssafy.tofy.plan.dto.PlanDetail;
+
+@Service
+public interface PlanService {
+	// 여행 계획 생성
+	public void createPlan(Plan plan) throws Exception;
+	// 여행 계획 수정
+	public void updatePlan(Plan plan) throws Exception;
+	// 여행 계획 리스트 조회
+	public List<Plan> listPlan(Map<String, String> param) throws Exception;
+	// 여행 계획 1개 조회
+	public Plan getPlan(int planNo) throws Exception;
+	// 여행 계획 삭제
+	public void deletePlan(int planNo) throws Exception;
+	// 여행 계획 조회수 증가
+	public void updateHit(int planNo) throws Exception;
+	// 여행 계획 상세 불러오기
+	public List<PlanDetail> getPlanDetail(int planNo)throws Exception;
+	// 여행 계획 상세 삭제
+	public void deletePlanDetail(int planNo)throws Exception;
+}
