9096d7ff6750b483cee6edaa2092fd66835f31f0
ki_mensoo <ekclstkfka5@gmail.com>
2023-05-23 12:48:51 +0900

    refactor: CORS 관련 설정 변경
    포트 번호 추가

--- src/main/java/com/ssafy/tofy/config/WebConfig.java
+++ src/main/java/com/ssafy/tofy/config/WebConfig.java
@@ -1,15 +1,19 @@
 package com.ssafy.tofy.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+@Configuration
+@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**").allowedOrigins("*")
-//		.allowedOrigins("http://localhost:8080", "http://localhost:8081")
+		.allowedOrigins("http://localhost:8080","http://localhost:8081","http://localhost:8082","http://localhost:8083", "http://localhost:8099")
                 .allowedMethods(HttpMethod.GET.name(), HttpMethod.POST.name(), HttpMethod.PUT.name(),
                         HttpMethod.DELETE.name(), HttpMethod.HEAD.name(), HttpMethod.OPTIONS.name(),
                         HttpMethod.PATCH.name())
