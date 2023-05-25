d4a82a36c1d045d8f85c557a5551e4f7bd8caf85
KodaHye <kodhdh09@gmail.com>
2023-05-25 11:20:22 +0900

    refactor: 공지사항 dto, mapper 수정

--- src/main/java/com/ssafy/tofy/notice/dto/NoticeDto.java
+++ src/main/java/com/ssafy/tofy/notice/dto/NoticeDto.java
@@ -1,15 +1,18 @@
 package com.ssafy.tofy.notice.dto;
 
-import lombok.Builder;
+import com.ssafy.tofy.user.dto.User;
+
 import lombok.Data;
 
 @Data
-@Builder
 public class NoticeDto {
+	
 	private String noticeNo;
 	private String userNo;
 	private String noticeTitle;
 	private String noticeContent;
 	private String noticeCreate;
 	private String noticeHit;
+	private User user;
+	
 }
